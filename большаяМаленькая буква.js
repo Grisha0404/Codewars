7a69c2b3ae0e988f1dd08ded8ac61026df4b2e4b
Grisha <grisha.kom@gmail.com>
2022-01-16 13:21:54 +0300

    sum

---
+++ большаяМаленькая буква.js
@@ -0,0 +1,10 @@
+// если с маленько одно с большой другое
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if (name[0].toLowerCase() === 'r'){
+      return name + " plays banjo";
+    }else{
+      return name + " does not play banjo"
+    }
+  }
+  
