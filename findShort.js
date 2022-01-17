8c9e2755976bf56892aac5bab8d896b3a33788e3
Grisha <grisha.kom@gmail.com>
2022-01-17 12:23:47 +0300

    Create findShort.js

---
+++ findShort.js
@@ -0,0 +1,8 @@
+function findShort(s){
+    const stringArray = s.split(" ");  // <-- no .trim()
+    const orderedArray = stringArray.sort((a, b) => {
+      return a.length - b.length;  // - instead of >
+    })
+    return orderedArray[0].length;
+  }
+  
